99bbd5f2e680a81b193409fa4eeb0b38858a70f6
it-william <william.cheng@kkday.com>
2018-12-21 12:01:45 +0800

    first commit

---
+++ LocalFramework/LocalFramework/LocalFramework.h
@@ -0,0 +1,19 @@
+//
+//  LocalFramework.h
+//  LocalFramework
+//
+//  Created by wei-tsung-cheng on 2018/12/21.
+//  Copyright © 2018 wei-tsung cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocalFramework.
+FOUNDATION_EXPORT double LocalFrameworkVersionNumber;
+
+//! Project version string for LocalFramework.
+FOUNDATION_EXPORT const unsigned char LocalFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalFramework/PublicHeader.h>
+
+
---
+++ LocalFramework/LocalFramework/Service.swift
@@ -0,0 +1,21 @@
+//
+//  Service.swift
+//  LocalFramework
+//
+//  Created by wei-tsung-cheng on 2018/12/21.
+//  Copyright © 2018 wei-tsung cheng. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+
+    private init() {
+
+    }
+
+    private static func dosomething() -> String {
+        return "Life is long, try your best to do anything"
+    }
+}
+
